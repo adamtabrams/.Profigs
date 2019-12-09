ea6e0e02ff7014b0dc0a2c6dbbb18c4a08951e4f
Adam Abrams <adam.abrams@optum.com>
2019-12-08 22:52:57 -0600

    updated my zsh plugins

--- config/zsh/jump.zsh
+++ config/zsh/jump.zsh
@@ -3,39 +3,47 @@
 # unmark FOO: delete a mark
 # marks: lists all marks
 
-export MARKFILE=$HOME/.local/share/lf/marks
-
-jump() {
-    if [ $(echo $1 | head -c 1) == ~ ]; then cd "$1"
-    elif [ $(echo $1 | head -c 2) == // ]; then cd $(echo "$1" | sed "s:\/\/:$HOME\/:")
-    elif [ $(echo $1 | head -c 1) == / ]; then cd "$1"
-    elif [ $(echo -n $1 | wc -c) -gt 1 ]; then cd "$1"; fi
-
-    MARKPATH=$(grep --color=never "^$1:" "$MARKFILE")
-    MARKPATH=${MARKPATH:2}
-    cd "$MARKPATH" 2>/dev/null || {echo "No such mark: $1"; return 1}
-}
-
-mark() {
-    if [ ${#1} != 1 ]; then
-        echo "Must be 1 char"
-        return 1
-    fi
-
-    if grep "^$1:" "$MARKFILE"; then
-        echo "Char already assigned"
-        return 1
-    fi
-
-    PWD=$(pwd)
-    echo "$1:$PWD" >> "$MARKFILE"
-}
-
-unmark() {
-    sed -i '' -e "/^$1:/d" "$MARKFILE"
-}
+export MARKFILE="$HOME/.local/share/lf/marks"
 
 marks() {
     cat "$MARKFILE" | tr ':' ' '
 }
 
+jump() {
+    if [[ $(echo $1 | head -c 1) == ~ ]]; then cd "$1"; return 0
+    elif [[ $(echo $1 | head -c 1) == / ]]; then cd "$1"; return 0
+    elif [[ $(echo $1 | head -c 1) == . ]]; then cd "$1"; return 0
+    elif [[ $(echo -n $1 | wc -c) -gt 1 ]]; then cd "$1"; return 0
+    fi
+
+    markline=$(grep --color=never "^${1}:" "$MARKFILE")
+    markpath=${markline:2}
+
+    if [[ $markpath ]]; then
+        cd "$markpath"
+    else
+        echo "No such mark: $1"
+        marks
+    fi
+}
+
+mark() {
+    if [[ $# != 1 ]]; then
+        echo "Must be 1 char"
+        return 1
+    fi
+
+    if grep "^${1}:" "$MARKFILE"; then
+        echo "Char already assigned"
+        return 1
+    fi
+    echo "${1}:$PWD" >> "$MARKFILE"
+}
+
+unmark() {
+    if [[ $(uname -s) == Darwin ]]; then
+        sed -i '' -e "/^${1}:/d" "$MARKFILE"
+    else
+        sed -i "/^${1}:/d" "$MARKFILE"
+    fi
+}
--- config/zsh/venv.zsh
+++ config/zsh/venv.zsh
@@ -1,14 +1,12 @@
 venv() {
-    if [ "$CUR_VENV" ]; then
-        echo "Deactivating $CUR_VENV/env"
+    if [[ $VIRTUAL_ENV ]]; then
+        echo "Deactivating $VIRTUAL_ENV/venv"
         deactivate 2>/dev/null
-        export CUR_VENV=""
     fi
 
-    if [ -d ./env ]; then
-        echo "Sourcing $PWD/env"
-        source ./env/bin/activate
-        export CUR_VENV="$PWD"
+    if [[ -d ./venv ]]; then
+        echo "Sourcing $PWD/venv"
+        source ./venv/bin/activate
     fi
 }
 
