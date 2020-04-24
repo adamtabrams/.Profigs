62ac64cc2c96992cbad568e187e624a7c090017b
Adam Abrams <adam.abrams@optum.com>
2020-04-24 16:51:27 -0500

    backup lazydock improve lazy link script

--- other/link-lazydocker-config.sh
+++ other/link-lazydocker-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apple_path="${HOME}/Library/Application Support/jesseduffield/lazygit"
-desired_path="${HOME}/.Profigs/config/jesseduffield/lazygit"
+apple_path="${HOME}/Library/Application Support/jesseduffield/lazydocker"
+desired_path="${HOME}/.Profigs/config/jesseduffield/lazydocker"
 
 temp_dir=$(mktemp -d)
 cp -r "${apple_path}/" "${temp_dir}"
